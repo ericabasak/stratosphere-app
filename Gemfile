c87fbfd059e59ab30cc9e6ea844bd554bf520ced
Erica Basak <ericabasak@gmail.com>
2021-11-05 13:07:46 -0400

    working code

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'bootstrap', '~> 5.1'
 gem 'jquery-rails'
+gem 'json', '~> 2.6', '>= 2.6.1'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,33 @@ class HomeController < ApplicationController
     require 'net/http'
     require 'json'
     @url = "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=94578&distance=25&API_KEY=CCA0A243-7978-4097-B069-B87C35F2930C"
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @output = JSON.parse(@response)
+
+
+    if @output.empty?
+      @final_output = "error"
+    else
+      @final_output = @output[0]["AQI"]
+    end
+
+    if @final_output == "error"
+      @api_color = "grey"
+    elsif @final_output <= 50
+      @api_color = "green"
+    elsif @final_output >= 51 && @final_output <= 100
+      @api_color = "yellow"
+    elsif @final_output >= 101 && @final_output <= 150
+      @api_color = "orange"
+    elsif @final_output >= 151 && @final_output <= 200
+      @api_color = "red"
+    elsif @final_output >= 201 && @final_output <= 300
+      @api_color = "purple"
+    elsif @final_output >= 301 && @final_output <= 500
+      @api_color = "maroon"
+    end
+      
   end
+
 end
