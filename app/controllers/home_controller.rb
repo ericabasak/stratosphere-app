8212341984f2631570f97db895010be8ce606193
Erica Basak <ericabasak@gmail.com>
2021-11-05 00:05:46 -0400

    changes to the home controller and index html and added bootstrap for styling which included navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+    @url = "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=94578&distance=25&API_KEY=CCA0A243-7978-4097-B069-B87C35F2930C"
   end
 end
